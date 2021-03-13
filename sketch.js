121ce97cf905120d256362973cfe7950ff9f6ee0
ksk2147 <72691651+ksk2147@users.noreply.github.com>
2021-03-13 11:56:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,11 @@ const Bodies = Matter.Bodies
 
 var engine, world;
 var ground;
+
 var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12;
+var box13, box14, box15, box16, box17, box18, box19, box20, box21, box22, box23, box24;
+var box25, box26, box27, box28, box29, box30, box31, box32, box33, box34, box35, box36;
+
 var ball;
 var rope;
 
@@ -28,8 +32,32 @@ function setup(){
     box10 = new Box(500, 100)
     box11 = new Box(500, 100)
     box12 = new Box(500, 100)
-    
 
+    box13 = new Box(800, 100)
+    box14 = new Box(800, 100)
+    box15 = new Box(800, 100)
+    box16 = new Box(800, 100)
+    box17 = new Box(800, 100)
+    box18 = new Box(800, 100)
+    box19 = new Box(700, 100)
+    box20 = new Box(700, 100)
+    box21 = new Box(700, 100)
+    box22 = new Box(700, 100)
+    box23 = new Box(700, 100)
+    box24 = new Box(700, 100)
+
+    box25 = new Box(200, 100)
+    box26 = new Box(200, 100)
+    box27 = new Box(200, 100)
+    box28 = new Box(200, 100)
+    box29 = new Box(200, 100)
+    box30 = new Box(200, 100)
+    box31 = new Box(100, 100)
+    box32 = new Box(100, 100)
+    box33 = new Box(100, 100)
+    box34 = new Box(100, 100)
+    box35 = new Box(100, 100)
+    box36 = new Box(100, 100)
 }
 
 function draw(){
@@ -52,6 +80,32 @@ function draw(){
     box10.display();
     box11.display();
     box12.display();
+
+    box13.display();
+    box14.display();
+    box15.display();
+    box16.display();
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+    box21.display();
+    box22.display();
+    box23.display();
+    box24.display();
+
+    box25.display();
+    box26.display();
+    box27.display();
+    box28.display();
+    box29.display();
+    box30.display();
+    box31.display();
+    box32.display();
+    box33.display();
+    box34.display();
+    box35.display();
+    box36.display();
 
 }
 
